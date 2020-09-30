eba0bb0cad2d97e05e0b66e424b5afc56821e1e5
shijundai <shijund@opera.com>
2020-09-30 18:37:17 +0800

    change to weighted sample without weighted loss

--- pbert_smedia_sigmoid.py
+++ pbert_smedia_sigmoid.py
@@ -128,7 +128,7 @@ class BertClassification(BertPreTrainedModel):
         # print('++debug type : logits: {}\nlabel_ids:{}'.format(type(logits), type(label_ids)))
 
         if label_ids is not None:
-            loss_fct = BCEWithLogitsLoss(pos_weight=torch.FloatTensor([8]).to(device))
+            loss_fct = BCEWithLogitsLoss()
             return loss_fct(logits, label_ids.view(-1,1))
         return logits
 
@@ -237,7 +237,7 @@ def main(bert_model='bert-base-cased', cache_dir=None,
 
         if valid_loss<=valid_losss_min:
             print('Validation loss decreased ({:.6f}-->{:.6f}) Saving model ...'.format(valid_losss_min, valid_loss))
-            torch.save(model.state_dict(), 'data/cache/bert_finetune_sig_weightpos_weightsamp_checkp.pt')
+            torch.save(model.state_dict(), 'data/cache/bert_finetune_sig_weightsamp_checkp.pt')
             valid_losss_min = valid_loss
 
         early_stopping(valid_loss, model)
@@ -246,8 +246,8 @@ def main(bert_model='bert-base-cased', cache_dir=None,
             print('Early Stopping: {}'.format(epoch))
             break
 
-    model.load_state_dict(torch.load('data/cache/bert_finetune_sig_weightpos_weightsamp_checkp.pt'))
-    torch.save(model, 'data/cache/model_smedia_sig_weightpos_weightsamp_smedia')
+    model.load_state_dict(torch.load('data/cache/bert_finetune_sig_weightsamp_checkp.pt'))
+    torch.save(model, 'data/cache/model_smedia_sig_weightsamp_smedia')
     print('bert fine-tune ok')
 
 if __name__ == '__main__':
