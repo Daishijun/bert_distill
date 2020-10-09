2789cf8e0125a937babb014a49ea4c8742af59f2
shijundai <shijund@opera.com>
2020-10-09 21:00:50 +0800

    save to cache/cpucache cpu

--- checkCPUmodels/checkcpumodel.py
+++ checkCPUmodels/checkcpumodel.py
@@ -16,12 +16,12 @@ device = torch.device('cpu')
 
 from transformers import BertTokenizer
 from transformers import BertModel, BertPreTrainedModel
-from torch.nn import CrossEntropyLoss
+from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
 
 FTensor = torch.FloatTensor
 
 class BertClassification(BertPreTrainedModel):
-    def __init__(self, config, num_labels=2):
+    def __init__(self, config, num_labels=1):
         super(BertClassification, self).__init__(config)
         self.num_labels = num_labels
         self.bert = BertModel(config)
@@ -34,19 +34,19 @@ class BertClassification(BertPreTrainedModel):
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         if label_ids is not None:
-            loss_fct = CrossEntropyLoss()
-            return loss_fct(logits.view(-1, self.num_labels), label_ids.view(-1))
+            loss_fct = BCEWithLogitsLoss()
+            return loss_fct(logits, label_ids.view(-1, 1))
         return logits
 
 if __name__ == '__main__':
 
     newmodel = BertClassification.from_pretrained('bert-base-cased',
-                                               cache_dir=None, num_labels=2)
+                                               cache_dir=None, num_labels=1)
     newmodel.to(device)
     print('load resave params ...')
-    newmodel.load_state_dict(torch.load('../data/cache/resaved_params.pth', map_location=torch.device('cpu')))
+    newmodel.load_state_dict(torch.load('../data/cache/resaved_params_sig_weightedpos.pth', map_location=torch.device('cpu')))
     print('load ok')
 
     print('save to cpu')
-    torch.save(obj=newmodel.state_dict(), f='../data/cache/cpucache/resaved_params_cpu.path')
+    torch.save(obj=newmodel.state_dict(), f='../data/cache/cpucache/resaved_params_sig_weightedpos.pth')
     print('save to cpu ok')
