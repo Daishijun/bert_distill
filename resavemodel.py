b39890aea2ddde571564e18634c389a51ca551ef
shijundai <shijund@opera.com>
2020-10-09 20:28:58 +0800

    change torch model to onnx for sigmoid & weighted pos model

--- resavemodel.py
+++ resavemodel.py
@@ -9,9 +9,9 @@ Introduction:
 
 import torch
 from transformers import BertTokenizer
-from ptbert_smedia import BertClassification
+# from ptbert_smedia import BertClassification
 from transformers import BertModel, BertPreTrainedModel
-from torch.nn import CrossEntropyLoss
+from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
 from small import *
 from utils_smedia import *
 import time
@@ -30,9 +30,10 @@ class Teacher(nn.Module):
         self.tokenizer = BertTokenizer.from_pretrained(
             bert_model, do_lower_case=True)
         # self.model = torch.load('./data/cache/model_smedia_smedia')  #加载预训练好的bert
-        self.model = torch.load('./data/cache/model_smedia_smedia_epoch20')  #加载预训练好的bert 20个epoch的
+        # self.model = torch.load('./data/cache/model_smedia_smedia_epoch20')  #加载预训练好的bert 20个epoch的
         # self.model = torch.load('./data/cache/model_smedia_smedia_earlyS')  #加载预训练好的bert  early stop patience==3, 结果就保存了第一个。
         # self.model = torch.load('./data/cache/model_smedia_smedia_earlyS_E50P5')  #加载预训练好的bert  early stop patience==5, 最多50个epoch。
+        self.model = torch.load('./data/cache/model_smedia_sig_weightsamp_smedia')
         self.model.eval()  #只做预测不再调参
 
     def predict(self, text):
@@ -43,11 +44,11 @@ class Teacher(nn.Module):
         input_ids = torch.tensor([input_ids + padding], dtype=torch.long).to(device)
         input_mask = torch.tensor([input_mask + padding], dtype=torch.long).to(device)
         logits = self.model(input_ids, input_mask, None)
-        return F.softmax(logits, dim=1).detach().cpu().numpy()
+        return F.sigmoid(logits).detach().cpu().numpy()
 
 
 class BertClassification(BertPreTrainedModel):
-    def __init__(self, config, num_labels=2):
+    def __init__(self, config, num_labels=1):
         super(BertClassification, self).__init__(config)
         self.num_labels = num_labels
         self.bert = BertModel(config)
@@ -60,18 +61,19 @@ class BertClassification(BertPreTrainedModel):
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         if label_ids is not None:
-            loss_fct = CrossEntropyLoss()
-            return loss_fct(logits.view(-1, self.num_labels), label_ids.view(-1))
+            loss_fct = BCEWithLogitsLoss()
+            return loss_fct(logits, label_ids.view(-1, 1))
         return logits
 
 if __name__ == '__main__':
     teacher = Teacher()
     print('save model params start')
-    torch.save(obj=teacher.model.state_dict(), f='data/cache/resaved_params.pth')
+    torch.save(obj=teacher.model.state_dict(), f='data/cache/resaved_params_sig_weightedpos.pth')
     print('resave ok')
     #
     newmodel = BertClassification.from_pretrained('bert-base-cased',
-                                               cache_dir=None, num_labels=2)
+                                               cache_dir=None, num_labels=1)
     print('load resave params ...')
-    newmodel.load_state_dict(torch.load('data/cache/resaved_params.pth'))
+    newmodel.load_state_dict(torch.load('data/cache/resaved_params_sig_weightedpos.pth'))
     print('load ok')
+
