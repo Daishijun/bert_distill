0cef23aca6fd3e744a6f1251b5789bf502dbf5b1
shijundai <shijund@opera.com>
2020-10-12 12:10:12 +0800

    debug model v3, test_model_pri_sig.py check load params not model

--- test_model_pri_sig.py
+++ test_model_pri_sig.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
                                                   cache_dir=None, num_labels=1)
     newmodel.to(device)
     print('load resave params ...')
-    newmodel.load_state_dict(torch.load('../data/cache/cpucache/resaved_params_sig_weightedpos.pth'))
+    newmodel.load_state_dict(torch.load('/data/cache/cpucache/resaved_params_sig_weightedpos.pth'))
     print('load ok')
     teacher = Teacher2(trainedmodel=newmodel)
 
